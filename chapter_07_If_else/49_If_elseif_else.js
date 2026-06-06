b5da794e5135c05309d75ce0ec1f42f5e2b4f495
Ankit Kumar Singh <ankitks04@users.noreply.github.com>
2026-06-06 23:00:29 +0530

    if else revision update

---
+++ chapter_07_If_else/49_If_elseif_else.js
@@ -0,0 +1,15 @@
+let Score = 58;
+
+if (Score >= 90) {
+    console.log("A");
+} else if (Score >= 80) {
+    console.log("B");
+} else if (Score >= 70) {
+    console.log("C");
+} else if (Score >= 60) {
+    console.log("D");
+} else {
+    console.log("F- Fail");
+    console.log("Rewatch all the video and give the test again");
+
+}
---
+++ chapter_07_If_else/50_REAL_IF_ELSE.js
@@ -0,0 +1,7 @@
+let age = 14;
+if (age >= 18) {
+    console.log("You are adult");
+  
+} else {
+    console.log("You are a minor");
+}
